1d03770c929afc1cae868c6a7f5d607806dc2964
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-07-28 22:49:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function takeANumber(queue, new_person) {
   return `Welcome, ${new_person}. You are number ${queue.length} in line.`
 }
 function nowServing(queue) {
-  queue.shift();
   if(queue.length === 0) {return "There is nobody waiting to be served!";}
   else {return `Currently serving ${queue[0]}.`;}
+  queue.shift();
 }
 function currentLine(queue) {}
